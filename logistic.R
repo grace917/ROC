d99b180ea17ceb45c8747ff5a3c605d885575059
grace917 <87958060+grace917@users.noreply.github.com>
2022-09-06 11:44:16 +0800

    fit, predict and plot ROC curve

--- logistic.R
+++ logistic.R
@@ -29,26 +29,3 @@ library(ROCR)
 predtrn<-prediction(pred, test$Y)
 performance(predtrn, "auc")@y.values[[1]]
 plot(perfromance(predtrn, "tpr", "fpr"), main="ROC")
-
-
-
-
-
-
-
-
-
-#run on smaller sample
-test<-raw_data[c(1:10),]
-set.seed(1)
-n<-nrow(test)   #number of test's number of row
-Test<-runif(n,0,1)>0.6   #generate n random number using uniform[0,1], return "TRUE" if this random number>0.6 and "False" else, give this stream of "TRUE/FALSE" vector to Test 
-#runif(10,0,1)>0.6
-#>[1] FALSE  TRUE FALSE FALSE  TRUE FALSE FALSE  TRUE FALSE FALSE
-#cannot add ":" in the y, like [Indicator==FALSE,:] 
-
-
-
- 
-
-
